41da82cb4eab408f773e23ce9527dc19458e0261
lyndonpett <lyndonpett98@gmail.com>
2022-04-04 12:33:38 -0500

    Adding task 11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: the tree
+ *
+ * Return: if null 0, otherwise the size
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if(!(tree))
+	{
+		return (0);
+	}
+
+	return (binary_tree_size(tree->right) + 1 + binary_tree_size(tree->left));
+}
